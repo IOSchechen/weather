144956c7d412398a9a19c939a552d265a1aac422
shamkhanprog <sham9541@icloud.com>
2024-09-05 22:24:48 +0500

    Initial Commit

---
+++ weather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather
+//
+//  Created by Шамхан Дандаев on 05.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
